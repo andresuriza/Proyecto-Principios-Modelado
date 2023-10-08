d01148fd03d93f44227391d774edb2ff3363374e
Andres Uriza <urizandres@gmail.com>
2023-10-08 10:58:56 -0600

    File structure changes

---
+++ Interfaz proyecto/Interfaz proyecto/SelectLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz_proyecto
+{
+    public partial class SelectLine : Form
+    {
+        public SelectLine()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectLine_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            var r1 = new RegistradoEmp();
+            this.Visible = false;
+            r1.Show();
+        }
+    }
+}
