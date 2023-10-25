2892faf95876ca1de1c728f7130358c0a92c11e5
Andres u <urizandres@estudiantec.cr>
2023-10-24 20:58:30 -0600

    Cambio de framework a .NET 7

---
+++ TabletUI/TabletUI/PantallaIngreso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabletUI
+{
+    public partial class PantallaIngreso : Form
+    {
+        public PantallaIngreso()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = false;
+            var w1 = new SelectLine();
+            w1.Show();
+        }
+    }
+}
